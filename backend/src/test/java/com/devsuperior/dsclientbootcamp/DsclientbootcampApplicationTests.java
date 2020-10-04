6bcc6ebaf505f0c1f5a4fa48baddb3572a371241
Rubens Brock <r.brocksilva@yahoo.com.br>
2020-10-04 17:25:09 -0300

    Project dsclient created

---
+++ backend/src/test/java/com/devsuperior/dsclientbootcamp/DsclientbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclientbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientbootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
